882fa0bc880eedc8498c4ad5e4562f234da3d3f0
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2022-12-08 00:45:31 +0100

    playermovement beginning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float currentPlayerSpeed = 1f;
+    private float addPlayerSpeed = 0.1f;
+    private float waitTime = 3f;
+    private float jumpForce = 10f;
+    private float laneChangeDistance = 10f;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        StartCoroutine(AddPlayerSpeedOverTime(waitTime));
+        PlayerLeftAndRight();
+    }
+
+    private IEnumerator AddPlayerSpeedOverTime(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            currentPlayerSpeed += addPlayerSpeed;
+        }
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("The player jumped");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void PlayerLeftAndRight()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * laneChangeDistance;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * laneChangeDistance;
+        }
+
     }
 }
